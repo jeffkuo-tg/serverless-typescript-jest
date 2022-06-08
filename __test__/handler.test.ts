a8bec90c81cf4f8acd7beb6debfaa47621e471af
jeff.kuo <jeff.kuo@tigergraph.com>
2022-06-08 18:51:59 -0500

    add mock module test

--- __test__/handler.test.ts
+++ __test__/handler.test.ts
@@ -1,11 +1,21 @@
-import { getUser } from "../handler";
+import { getUser } from '../handler';
+import axios from 'axios';
+
+jest.mock('axios'); // need to placed in the same scope as import statement
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('Test handler functions', () => {
-    test('Test getUaser', async () => {
-        const user1 = await getUser(1);
-        expect(user1.name).toEqual('Leanne Graham');
+    test('Test getUaser with axios mocked', async () => {
+        const userName = {
+            name: 'Leanne Graham'
+        };
 
-        const user2 = await getUser(2);
-        expect(user2.name).not.toEqual('Leanne Graham');
-      });
+        const response = {
+            data: userName
+        };
+
+        mockedAxios.get.mockResolvedValueOnce(response); // return {data: {name: 'Leanne Graham'}}
+        const user1 = await getUser(2); // even user2's name is not Leanne Graham, we still pass the test
+        expect(user1.name).toEqual('Leanne Graham');
+    });
 });
